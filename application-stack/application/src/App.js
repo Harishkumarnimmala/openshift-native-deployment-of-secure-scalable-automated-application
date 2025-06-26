33e9a702a1617077fb38e03c4ad765a269f6817f
Harish Kumar Nimmala <Harishkumar.nimmala@gmail.com>
2025-06-26 23:31:54 +0200

    Update frontend to show Hello message

--- application-stack/application/src/App.js
+++ application-stack/application/src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function App() {
-  return <h1>Hello from React frontend on OpenShift!</h1>;
+  return (
+    <div>
+      <h1>Hello from React frontend on OpenShift!</h1>
+    </div>
+  );
 }
 
 export default App;
